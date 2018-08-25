e737b7382c9c34a7f833940c1d484bf8da94a6d6
chessai <chessai1996@gmail.com>
2018-08-25 15:50:34 -0400

    stop converting to base's maybe type

--- src/Data/Maybe/Unpacked/Numeric/Word.hs
+++ src/Data/Maybe/Unpacked/Numeric/Word.hs
@@ -16,26 +16,26 @@ module Data.Maybe.Unpacked.Numeric.Word
 
 import Prelude hiding (Maybe,maybe)
 
-import GHC.Exts (Word#,(*#),(+#),and#,indexWordArray#,readWordArray#)
+import GHC.Base (build)
+import GHC.Exts (Word#,(*#),(+#),and#,indexWordArray#,readWordArray#,word2Int#, (==#))
 import GHC.Word (Word(W#))
 import Data.Primitive.Types (Prim(..))
+
+import GHC.Read (Read(readPrec), expectP)
+import Text.Read (parens, Lexeme(Ident), lexP, (+++))
+import Text.ParserCombinators.ReadPrec (prec, step)
 
 import qualified Prelude as P
 
 data Maybe = Maybe (# (# #) | Word# #)
 
 instance Eq Maybe where
-  a == b = toBaseMaybe a == toBaseMaybe b
-  a /= b = toBaseMaybe a /= toBaseMaybe b
-
+  ma == mb =
+    maybe (isNothing mb)
+          (\a -> maybe False (\b -> a == b) mb) ma
+    
 instance Ord Maybe where
-  compare a b = compare (toBaseMaybe a) (toBaseMaybe b)
-  a > b = toBaseMaybe a > toBaseMaybe b
-  a < b = toBaseMaybe a < toBaseMaybe b
-  a >= b = toBaseMaybe a >= toBaseMaybe b
-  a <= b = toBaseMaybe a <= toBaseMaybe b
-  min a b = fromBaseMaybe (min (toBaseMaybe a) (toBaseMaybe b))
-  max a b = fromBaseMaybe (max (toBaseMaybe a) (toBaseMaybe b))
+  compare ma mb = maybe LT (\a -> maybe GT (compare a) mb) ma  
 
 instance Show Maybe where
   showsPrec p (Maybe m) = case m of
@@ -43,6 +43,17 @@ instance Show Maybe where
     (# | w #) -> showParen (p > 10)
       $ showString "just "
       . showsPrec 11 (W# w)
+
+instance Read Maybe where
+  readPrec = parens $ nothingP +++ justP
+    where
+      nothingP = prec 10 $ do
+        Ident "nothing" <- lexP
+        return nothing
+      justP = prec 10 $ do
+        Ident "just" <- lexP
+        a <- step readPrec
+        return (just a)
 
 instance Prim Maybe where
   sizeOf# _ = sizeOf# (undefined :: Word) *# 2#
@@ -57,6 +68,39 @@ instance Prim Maybe where
       0## -> (# s1, nothing #)
       _ -> case readWordArray# arr ((2# *# ix) +# 1#) s1 of
         (# s2, y #) -> (# s2, Maybe (# | y #) #)
+
+listToMaybe :: [Word] -> Maybe
+listToMaybe [] = nothing
+listToMaybe (x:_) = just x
+
+maybeToList :: Maybe -> [Word]
+maybeToList = maybe [] (: [])
+
+catMaybes :: [Maybe] -> [Word]
+catMaybes = mapMaybe P.id
+
+mapMaybe :: (a -> Maybe) -> [a] -> [Word]
+mapMaybe _ [] = []
+mapMaybe f (a : as) =
+  let ws = mapMaybe f as
+  in maybe ws (: ws) (f a)
+{-# NOINLINE [1] mapMaybe #-}
+
+{-# RULES
+"mapMaybe"     [~1] forall f xs. mapMaybe f xs
+                    = build (\c n -> foldr (mapMaybeFB c f) n xs)
+"mapMaybeList" [1]  forall f. foldr (mapMaybeFB (:) f) [] = mapMaybe f
+  #-}
+
+{-# NOINLINE [0] mapMaybeFB #-}
+mapMaybeFB :: (Word -> r -> r) -> (a -> Maybe) -> a -> r -> r
+mapMaybeFB cons f x next = maybe next (flip cons next) (f x)
+
+isNothing :: Maybe -> Bool
+isNothing = maybe True (const False)
+
+isJust :: Maybe -> Bool
+isJust = maybe False (const True)
 
 nothing :: Maybe
 nothing = Maybe (# (# #) | #)
